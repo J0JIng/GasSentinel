bdfa301a1e2b8bf392369a0cc7f2e3d01f5b5754
Jing <111691710+J0JIng@users.noreply.github.com>
2023-06-21 14:40:48 +0800

    Update server_resource_handler.py

--- Bridge Script/server_resource_handler.py
+++ Bridge Script/server_resource_handler.py
@@ -9,9 +9,8 @@ import tinytuya
 from server_sv_manager import ServerManager
 from user_handler import user_handler_callback
 
-
 class ResourceHandler(resource.Resource):
-    """This resource supports the PUT method.
+    """This resource supports the PUT methods.
     PUT: Update state of alarm."""
 
     def __init__(self, uri, sv_mgr: ServerManager):
@@ -23,30 +22,22 @@ class ResourceHandler(resource.Resource):
 
     async def render_put(self, request):
         """ Handles PUT requests, updates info, and calls functions to trigger actions. """
+
         client_ip = request.remote.hostinfo
         self.coap_payload = request.payload.decode("utf-8")
         csv = self.coap_payload.split(",")
-
         logging.info("Received PUT request from " + str(client_ip) + " with payload " + str(csv))
         logging.warning(csv)
-
         try:
-            token = csv[0]
-            if token != self.sv_mgr._general_token:  # Check if token matches the private token in ServerManager
-                raise ValueError("Invalid token")
-        except (IndexError, ValueError):
-            logging.warning("Invalid token or missing general token")
-            return aiocoap.Message(code=aiocoap.UNAUTHORIZED)
+            # Place them into a queue to be added into resource tree
+            ServerManager.incoming_queue_res_child_ips.add(ipaddress.ip_address(re.sub(r"[\[\]]", "", client_ip)))
 
-        # Continue processing if the token is valid
-        try:
-            if csv[1] == "0":  # if it is OdourGuard
-                self.sv_mgr.update_child_device_info(ipaddress.ip_address(re.sub(r"[\[\]]", "", client_ip)), csv)
-            else:
-                raise ValueError("Invalid payload")
-        except (IndexError, ValueError):
+            # Update the resource tree with client information
+            self.sv_mgr.update_child_uri()
+
+            """Work in progress"""
+            # Update the information on Client
+            self.sv_mgr.update_child_device_info(ipaddress.ip_address(re.sub(r"[\[\]]", "", client_ip)), csv)
+        except ValueError:
             logging.warning("Invalid payload")
-            return aiocoap.Message(code=aiocoap.BAD_REQUEST)
-
         user_handler_callback(ipaddress.ip_address(re.sub(r"[\[\]]", "", client_ip)), csv)
-        return aiocoap.Message(code=aiocoap.CON)
